77b76e64aa60b7f1cc83400e08dd2a7edfefb92a
Tamisimi <alexmd0107@gmail.com>
2025-07-02 12:41:37 +0700

    baitap

---
+++ PTIT_CNTT4_IT201_Session08/PTIT_CNTT4_IT201_Session08_Bai06.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[500], R[500];
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[left + i];
+    }
+    for (int i = 0; i < n2; i++) {
+        R[i] = arr[mid + 1 + i];
+    }
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void sort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        sort(arr, left, mid);
+        sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int n;
+    printf("Nhap so luong phan tu: ");
+    scanf("%d", &n);
+
+    if (n <= 0 || n >= 1000) {
+        printf("So luong phan tu khong hop le!\n");
+        return 0;
+    }
+
+    int arr[1000];
+    printf("Nhap %d phan tu:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("Phan tu thu %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    sort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+    }
+
+    return 0;
+}
