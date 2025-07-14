90fa13896ab2cee485e343a37ce80de1c2d9ea0f
Tamisimi <alexmd0107@gmail.com>
2025-07-14 23:24:17 +0700

    baitap

---
+++ PTIT_CNTT4_IT201_Session016/PTIT_CNTT4_IT201_Session016_Bai5.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+typedef struct Queue {
+    Node* front;
+    Node* rear;
+} Queue;
+
+Queue* initQueue() {
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue->front = NULL;
+    queue->rear = NULL;
+    return queue;
+}
+
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void enqueue(Queue* queue, int value) {
+    Node* newNode = createNode(value);
+    
+    if (queue->front == NULL) {
+        queue->front = newNode;
+        queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
+}
+
+int peek(Queue* queue) {
+    if (queue->front == NULL) {
+        printf("empty queue\n");
+        return -1;
+    }
+    return queue->front->data;
+}
+
+int dequeue(Queue* queue) {
+    if (queue->front == NULL) {
+        printf("queue is empty\n");
+        return -1;
+    }
+    
+    Node* temp = queue->front;
+    int value = temp->data;
+    queue->front = queue->front->next;
+    
+    if (queue->front == NULL) {
+        queue->rear = NULL;
+    }
+    
+    free(temp);
+    return value;
+}
+
+void printQueue(Queue* queue) {
+    printf("queue = {\n");
+    printf("  front->");
+    Node* current = queue->front;
+    while (current != NULL) {
+        printf("%d", current->data);
+        if (current->next != NULL) {
+            printf("->");
+        }
+        current = current->next;
+    }
+    printf("->NULL\n");
+    
+    printf("  rear->");
+    if (queue->rear != NULL) {
+        printf("%d->NULL\n", queue->rear->data);
+    } else {
+        printf("NULL\n");
+    }
+    printf("}\n");
+}
+
+int main() {
+    Queue* queue = initQueue();
+    
+    printQueue(queue);
+    dequeue(queue);
+    
+    enqueue(queue, 1);
+    enqueue(queue, 2);
+    enqueue(queue, 5);
+    printQueue(queue);
+    dequeue(queue);
+    printQueue(queue);
+    
+    while (queue->front != NULL) {
+        Node* temp = queue->front;
+        queue->front = queue->front->next;
+        free(temp);
+    }
+    free(queue);
+    
+    return 0;
+}
